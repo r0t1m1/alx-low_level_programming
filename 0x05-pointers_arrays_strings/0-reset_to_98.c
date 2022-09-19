40c122c103fb05571123969bd63b9f32b506fe3c
r0t1m1 <olatunbosun_r@yahoo.com>
2022-09-19 12:55:03 -0700

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets variable value to 98
+ * @n: pointer parameter
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * reset_to_98 - resets variabel to 98
+ * @n: pointer parameter
+ */
+void reset_to_98(int *n);
+#endif
