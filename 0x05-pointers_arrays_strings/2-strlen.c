cc453d47f26ec7ddcc8d27e16302b2be46101e29
r0t1m1 <olatunbosun_r@yahoo.com>
2022-09-19 13:48:47 -0700

    _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen = return string length
+ * @s: string parameter
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	return strlen(*s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,4 +11,10 @@ void reset_to_98(int *n);
  * @b: second integer
  */
 void swap_int(int *a, int *b);
+/**
+ * _strlen = returns string lenght
+ * #s: string parameter
+ * Return: string length
+ */
+int _strlen(char *s);
 #endif
