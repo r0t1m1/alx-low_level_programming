228174c46d61834b68c22db095d414ed8111c5dc
r0t1m1 <olatunbosun_r@yahoo.com>
2022-09-19 14:17:00 -0700

    fix bug

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
+#include "2-strlen.c"
 /**
  * print_rev - print reversed string
  * @s: string parameter
  */
 void print_rev(char *s)
 {
-	strrev(s);
-	printf("%s", s);
+	int i;
+	for (i=_strlen(s)-1; i>=0;i__) 
+	{
+		_putchar(*(s + i));
+	}
+	_putchar("\n");
+}
 }
