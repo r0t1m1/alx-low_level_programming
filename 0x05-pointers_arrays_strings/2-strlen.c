29b4a4d4807a5ad6b22a3cf30bbc3aa0aee7ddab
r0t1m1 <olatunbosun_r@yahoo.com>
2022-09-19 13:54:01 -0700

    fix bug

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strlen = return string length
+ * _strlen - return string length
  * @s: string parameter
  * Return: string length
  */
 int _strlen(char *s)
 {
-	return strlen(s);
+	return (strlen(s));
 }
