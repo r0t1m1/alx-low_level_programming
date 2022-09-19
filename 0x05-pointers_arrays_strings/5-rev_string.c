68f9c36a18d6c29d6d907e098d226b885b09b79a
r0t1m1 <olatunbosun_r@yahoo.com>
2022-09-19 14:49:18 -0700

    fix bug

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,18 @@
 #include "main.h"
-#include <string.h>
+#include "2-strlen.c"
 /**
  * rev_string - reverses string
  * @s: string parameter
  */
 void rev_string(char *s)
 {
-	strrev(s);
+	int i;
+	char t;
+
+	for (i = 0; i < _strlen(s) / 2; i++)
+	{
+		t = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = t;
+	}
 }
